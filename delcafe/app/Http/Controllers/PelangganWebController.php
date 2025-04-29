557fa74464cecde132889220d41e9177425a5b96
NicolasBatubara <149449407+NicolasBatubara@users.noreply.github.com>
2025-04-29 16:04:53 +0700

    controller pelanggan

---
+++ delcafe/app/Http/Controllers/PelangganWebController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pelanggan;
+use Illuminate\Http\Request;
+
+class PelangganWebController extends Controller
+{
+    public function index(Request $request)
+    {
+        $query = Pelanggan::query();
+
+        // Filter berdasarkan status
+        if ($request->filled('status')) {
+            $query->where('status', $request->status);
+        }
+
+        // Filter pencarian berdasarkan nama atau telepon
+        if ($request->filled('search')) {
+            $query->where(function ($q) use ($request) {
+                $q->where('nama', 'like', '%' . $request->search . '%')
+                  ->orWhere('telepon', 'like', '%' . $request->search . '%');
+            });
+        }
+
+        // Sorting
+        $sortBy = $request->get('sort_by', 'nama');
+        $sortOrder = $request->get('sort_order', 'asc');
+        $allowedSorts = ['nama', 'telepon', 'status'];
+
+        if (!in_array($sortBy, $allowedSorts)) {
+            $sortBy = 'nama';
+        }
+
+        $pelanggan = $query->orderBy($sortBy, $sortOrder)
+                           ->paginate(10)
+                           ->appends($request->all());
+
+        return view('pelanggan.index', compact('pelanggan', 'sortBy', 'sortOrder'));
+    }
+}
